9ed8b8b69d44c43af5f57a0e66db9a8b8183cef3
MJFullness <60741114+MJFullness@users.noreply.github.com>
2021-02-11 12:44:41 +0100

    Update bekk_functions.R

--- R/bekk_functions.R
+++ R/bekk_functions.R
@@ -187,3 +187,193 @@ valid_bekk <- function(C, A, G) {
 #   }
 #   return(sum(llv))
 # }
+
+hessian_BEKK=function(theta,r){
+
+  n=nrow(r)
+  N <- ncol(r)
+  N2 <- N^2
+  NoOfVars_C = N*(N+1)/2
+
+L_elimination <- elimination_mat(N)
+D_duplication <- duplication_mat(N)
+K_commutation <- commutation_mat(N)
+D_gen_inv <- solve(crossprod(D_duplication)) %*% t(D_duplication)
+
+
+C1=kronecker(kronecker(diag(N),K_commutation),diag(N))
+C2=2*(kronecker(diag(N2),D_duplication%*%D_gen_inv))
+C3=C2%*%C1
+
+#BEKK-Matrices
+c_mat <-  coef_mat(theta, 2)
+
+c0 <- c_mat$c0
+a <- c_mat$a
+g <- c_mat$g
+
+c_full=crossprod(c0)
+
+#often done calculations
+t_kron_g=t(kronecker(g,g))
+gt=t(g)
+kron_comm_cgt=kronecker(K_commutation,c(gt))
+
+
+#Hessian
+hessian = matrix(0, ncol = nrow(theta),nrow=nrow(theta))
+
+#Second derivatives for t=1
+dHdada = matrix(0,nrow=N2^2,ncol=N2) #Fehler bei h2??
+dHdadc = matrix(0,nrow=N2^2,ncol=NoOfVars_C)
+dHdadg=matrix(0,nrow=N2^2,ncol=N2)
+
+dHdgda = matrix(0,nrow=N2^2,ncol=N2)
+dHdgdg = matrix(0,nrow=N2^2,ncol=N2) #Fehler bei h2??
+dHdgdc = matrix(0,nrow=N2^2,ncol=NoOfVars_C)
+
+dHdcdc = matrix(0,nrow=NoOfVars_C*N2,ncol=NoOfVars_C)
+dHdcdg = matrix(0,nrow=NoOfVars_C*N2,ncol=N2)
+dHdcda = matrix(0,nrow=NoOfVars_C*N2,ncol=N2)
+
+# Partial derivatives for initial period t = 1
+ht <- crossprod(r)/nrow(r)
+
+dHda <- 2 * D_duplication %*% D_gen_inv %*% kronecker(diag(N), t(a) %*% ht)
+dHdg <- 2 * D_duplication %*% D_gen_inv %*% kronecker(diag(N), t(g) %*% ht)
+dHdc <- 2 * D_duplication %*% D_gen_inv %*% kronecker(t(c0), diag(N)) %*% t(L_elimination)
+
+dHdtheta <- t(cbind(dHdc, dHda, dHdg))
+
+
+ht_inv=solve(ht)
+
+dHHdc=cbind(dHdcdc,dHdcda,dHdcdg)
+dHHda=cbind(dHdadc,dHdada,dHdadg)
+dHHdg=cbind(dHdgdc,dHdgda,dHdgdg)
+
+dHHdtheta=rbind(dHHdc,dHHda,dHHdg)
+
+
+matt=matrix(0, nrow=nrow(theta),ncol=N2)
+matt[1,1]=1
+
+for ( i in 2:nrow(theta)){
+mat=matrix(0,nrow=nrow(theta),ncol=N2)
+mat[i,1]=1
+matt=cbind(matt,mat)
+}
+dHH=matt%*%dHHdtheta
+
+for (i in 2:N2){
+  matt=cbind(matrix(0,nrow=nrow(mat),ncol = 1),matt[,1:ncol(matt)-1])
+  dHH=rbind(dHH,matt%*%dHHdtheta)
+}
+
+for (i in 1:nrow(theta)){
+  for (j in 1:nrow(theta)){
+    dhi = t(dHdtheta[i,])
+    dhi = matrix(dhi,ncol=N,nrow=N);
+
+    dhj = t(dHdtheta[j,])
+    dhj = matrix(dhj,ncol=N,nrow=N);
+
+    temp = dHH[i,j];
+
+    for (k in 2:N2) {
+    temp = cbind(temp, dHH[(k-1)*nrow(theta)+i,j])
+    }
+
+    temp = matrix(temp,ncol=N, nrow=N)
+    ddh = t(temp)
+
+    mat = ddh%*%ht_inv-dhi%*%ht_inv%*%dhj%*%ht_inv+r[1,]%*%t(r[1,])%*%ht_inv%*%dhj%*%ht_inv%*%dhi%*%ht_inv-
+      r[1,]%*%t(r[1,])%*%ht_inv%*%ddh%*%ht_inv+r[1,]%*%t(r[1,])%*%ht_inv%*%dhi%*%ht_inv%*%dhj%*%ht_inv
+    #update hessian
+    hessian[i,j] = -0.5*sum(diag(mat))
+  }
+}
+
+#Second derivatives for t>1
+
+for (i in 2:n){
+  dHdada = C3%*%(kronecker(c(diag(N)),kronecker(r[i-1,]%*%t(r[i-1,]),diag(N)))%*%K_commutation)+kronecker(diag(N2),t_kron_g) %*%dHdada
+  dHdadg = kronecker(t(dHda), diag(N2))%*%C1%*%(kron_comm_cgt+kronecker(c(gt),K_commutation))+(kronecker(diag(N2),t(kronecker(g,gt))))%*%dHdadg
+  dHdadc = dHdadc # Always zero (row may be deleted later on)
+
+  dHdgda = C3%*%(kronecker(c(diag(N)),kronecker(diag(N),gt)%*%dHda ))+(kronecker(diag(N2),t_kron_g))%*%dHdgda
+  dHdgdg = C3%*%(kronecker(c(diag(N)),kronecker(ht,diag(N))%*%K_commutation+kronecker(diag(N),gt)%*%dHdg ))+kronecker(t(dHdg),diag(N2))%*%C1%*%(kron_comm_cgt+kronecker(c(gt),K_commutation))+kronecker(diag(N2),t_kron_g)%*%dHdgdg
+  dHdgdc = C3%*%kronecker(c(diag(N)),kronecker(diag(N),gt)%*%dHdc)+kronecker(diag(N2),t_kron_g)%*%dHdgdc
+
+  dHdcda = dHdcda # Always zero (row may be deleted later on)
+  dHdcdg = kronecker(t(dHdc),diag(N2))%*%C1%*%(kron_comm_cgt+kronecker(c(gt),K_commutation))+kronecker(diag(NoOfVars_C),t_kron_g)%*%dHdcdg
+  dHdcdc = 2*kronecker(L_elimination,D_duplication%*%D_gen_inv)%*%C1%*%kronecker(diag(N2),c(diag(N)))%*%t(L_elimination)+kronecker(diag(NoOfVars_C),t_kron_g)%*%dHdcdc
+
+  #updating first derivatives
+  dHda <- 2 * D_duplication %*% D_gen_inv %*% kronecker(diag(N), t(a) %*% r[(i-1), ] %*% t(r[(i-1), ])) + t(kronecker(g, g)) %*% dHda
+  dHdg <- 2 * D_duplication %*% D_gen_inv %*% kronecker(diag(N), gt %*% ht) + t(kronecker(g, g)) %*% dHdg
+  dHdc <- 2 * D_duplication %*% D_gen_inv %*% kronecker(t(c0), diag(N)) %*% t(L_elimination) + t(kronecker(g, g)) %*% dHdc
+
+  #update h
+  ht <- c_full + t(a) %*% r[(i-1), ] %*% t(r[(i-1), ]) %*% a + gt %*% ht %*%g
+
+  #Coputation of Hessian for each t>1
+
+
+  dHdtheta <- t(cbind(dHdc, dHda, dHdg))
+
+
+  ht_inv=solve(ht)
+
+  dHHdc=cbind(dHdcdc,dHdcda,dHdcdg)
+  dHHda=cbind(dHdadc,dHdada,dHdadg)
+  dHHdg=cbind(dHdgdc,dHdgda,dHdgdg)
+  dHHdtheta=rbind(dHHdc,dHHda,dHHdg)
+
+
+  matt=matrix(0, nrow=nrow(theta),ncol=N2)
+  matt[1,1]=1
+
+  for ( j in 2:nrow(theta)){
+    mat=matrix(0,nrow=nrow(theta),ncol=N2)
+    mat[j,1]=1
+    matt=cbind(matt,mat)
+  }
+  dHH=matt%*%dHHdtheta
+
+  for (j in 2:N2){
+    matt=cbind(matrix(0,nrow=nrow(mat),ncol = 1),matt[,1:ncol(matt)-1])
+    dHH=rbind(dHH,matt%*%dHHdtheta)
+  }
+
+
+
+  for (l in 1:nrow(theta)){
+    for (j in 1:nrow(theta)){
+      dhi = t(dHdtheta[l,])
+      dhi = matrix(dhi,ncol=N,nrow=N);
+
+      dhj = t(dHdtheta[j,])
+      dhj = matrix(dhj,ncol=N,nrow=N);
+
+      temp = dHH[l,j];
+
+      for (k in 2:N2) {
+        temp = cbind(temp, dHH[(k-1)*nrow(theta)+l,j])
+      }
+
+      temp = matrix(temp,ncol=N, nrow=N)
+      ddh = t(temp)
+      #get partial derivtatives for ll
+      mat = ddh%*%ht_inv-dhi%*%ht_inv%*%dhj%*%ht_inv+r[i,]%*%t(r[i,])%*%ht_inv%*%dhj%*%ht_inv%*%dhi%*%ht_inv-
+        r[i,]%*%t(r[i,])%*%ht_inv%*%ddh%*%ht_inv+r[i,]%*%t(r[i,])%*%ht_inv%*%dhi%*%ht_inv%*%dhj%*%ht_inv
+      #update hessian
+      hessian[l,j] = hessian[l,j]-0.5*sum(diag(mat))
+
+    }
+  }
+
+}
+
+return(hessian*(-1))
+}
