d7b7d8d4eddf3190e6305747e560bf72be12c6d8
MJFullness <markus.fuelle@gmail.com>
2021-02-11 15:50:15 +0100

    New version

--- tests/testthat/test-Grid_Search.R
+++ tests/testthat/test-Grid_Search.R
@@ -1,10 +1,8 @@
 test_that("Testing GridSearch works with TS_new", {
   set.seed(6723217)
-  gs1 <- random_grid_search_BEKK(BI,250)
+  gs1 <- random_grid_search_BEKK(data.matrix(BI),250)
 
   # Starting values should be equal for same seed
-  set.seed(6723217)
-  gs2 <- random_grid_search_BEKK(BI,250)
-  expect_identical(gs1[[1]], gs2[[1]])
 
+  expect_equal(gs1[[1]], c(0.19663233, -0.22513552,  0.57296538,  0.03252316,  0.01099530,  0.53818486, -0.42037775, 0.59263607,-0.26044763,  0.98807132, -0.05697966))
 })
