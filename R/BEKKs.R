aa15449655258bc28e5c502de9d5fcd72b7d9c8b
Alexander Lange <alexander.lange@uni-goettingen.de>
2021-06-07 15:48:30 +0200

    description update

--- R/BEKKs.R
+++ R/BEKKs.R
@@ -3,8 +3,16 @@
 #' @docType package
 #' @name BEKKs
 #' @author \itemize{
-#' \item Alexander Lange \email{alexander.lange@uni-goettingen.de}
+#' \item Markus Fülle  \email{fuelle@uni-goettingen.de}
 #' \item Helmut Herwartz \email{hherwartz@uni-goettingen.de}
+#' \item Alexander Lange \email{alexander.lange@uni-goettingen.de}
+#' }
+#' @description
+#' This package implements MGARCH estimation techniques for conditional volatility modelling.\cr
+#' @details
+#' The main functions to retrieve structural impact matrices are:
+#' \itemize{
+#' \item \tabular{ll}{ \code{\link{bekk}} \tab Estimates a BEKK(1,1,1) model,}
 #' }
 #' @useDynLib BEKKs
 #' @importFrom Rcpp sourceCpp
