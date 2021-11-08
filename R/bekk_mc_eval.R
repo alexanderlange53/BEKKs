93ae09343a6686e8a306bccc645740f2709393de
Alexander Lange <alexander.lange@uni-goettingen.de>
2021-11-08 14:59:39 +0100

    first commit for reorganisation

--- R/bekk_mc_eval.R
+++ R/bekk_mc_eval.R
@@ -8,12 +8,18 @@ bekk_mc_eval <- function(spec, sample_sizes, iter, nc = 1) {
     for(j in sample_sizes) {
       sim_dat <- vector(mode = "list", iter)
 
+      # for(i in 1:iter) {
+      #   sim_dat[[i]] <- bekk(bekk_sim(spec, j), nc =nc)
+      # }
+
       for(i in 1:iter) {
-        sim_dat[[i]] <- bekk(bekk_sim(spec, j), nc =nc)
+        sim_dat[[i]] <- bekk_sim(spec, j)
       }
 
+      dd <- pblapply(sim_dat, function(x){bekk(x)}, cl = nc)
 
-      mse[index] <- sum(unlist(lapply(sim_dat, RMSE, theta_true = theta)))/iter
+
+      mse[index] <- sum(unlist(lapply(dd, RMSE, theta_true = theta)))/iter
       index <- index +1
 
     }
@@ -36,4 +42,9 @@ bekk_mc_eval <- function(spec, sample_sizes, iter, nc = 1) {
 RMSE <- function(x, theta_true) {
   theta_est <- x$theta
   return(sum(sqrt(((theta_true - theta_est) / theta_true)^2)))
+}
+
+plot.bekkMC <- function(x, ...) {
+  msep <- x[[1]]
+  ggplot(msep) + geom_line(aes(x = Sample, y = MSE)) + theme_bw()
 }
---
+++ R/bekk_spec.R
@@ -0,0 +1,32 @@
+#' BEKK specification method
+#'
+#' @param model List containing the model type specification: Currently implemented are "bekk", "diagBekk", "scalarBekk".
+#' Moreover it can be specified whether the model should be estimated allowing for asymmetric volatility structure.
+#' @param init_values initial values for \href{bekk_fit} during BHHH algorithm. It can be either a numerical vector of suitable dimension, or a character vector i.e. "random" to use a random starting values generator, or
+#'  "simple" for relying on a simple initial values generator based on typical values for BEKK parameter found in the literature. If object from this function is passed to \link{bekk_sim}, init_values are used as parameters for data generating process.
+#' @param signs Vector specifying asymmetry.
+#' @param Integer specifying the dimension of the BEKK model. Only relevant for \link{bekk_sim}.
+#'
+#'
+#'
+#'
+#' @export
+
+
+bekk_spec <- function(model = list(type = "bekk", asymmetric = FALSE),
+                      init_values = NULL, signs = NULL, N = NULL) {
+
+
+  # Checking inputs
+  if(!is.null(N) & is.numeric(init_values)) {
+    if(length(init_values) != 2 * N^2 + N * (N + 1)/2) {
+      stop('Number of initial parameter does not match dimension of data.')
+    }
+  }
+
+  specification <- list(model, init_values, signs, N)
+  class(specification) <- 'bekkSpec'
+
+  return(specification)
+
+}
