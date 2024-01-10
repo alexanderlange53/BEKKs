7bad6737aec0c64bbca88f27f06093a9e09c1164
Markus <markus.fuelle@gmail.com>
2024-01-10 19:51:45 +0100

    bugfix runtime CRAN

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
+
+.onLoad <- function(libname, pkgname) {
+
+  # CRAN OMP THREAD LIMIT
+  Sys.setenv("OMP_THREAD_LIMIT" = 1)
+}
 .onUnload <- function(libpath) {
   library.dynam.unload("BEKKs", libpath)
   invisible(NULL)
